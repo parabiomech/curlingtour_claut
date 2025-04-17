ca5069945eb34c4a5a9bc0d68637d99fe6e0394d
parabiomech <parabiomech@gmail.com>
2025-04-17 10:40:38 +0200

    send

---
+++ data.js
@@ -0,0 +1,697 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-claut__",
+      "name": "Claut_외부_선수대기실출입문",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.0414015324930439,
+        "pitch": 0.08477811976970351,
+        "fov": 1.3737605190682325
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.14717220228586925,
+          "pitch": 0.33377065020142993,
+          "rotation": 0,
+          "target": "1-claut__"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.13810764239106454,
+          "pitch": -0.3401724754859945,
+          "title": "Claut curling center",
+          "text": "컬링장 외부"
+        }
+      ]
+    },
+    {
+      "id": "1-claut__",
+      "name": "Claut_내부_선수대기실입구",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.1424045341662712,
+        "pitch": 0.3442509008415833,
+        "fov": 1.3737605190682325
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.15900482013691963,
+          "pitch": 0.35897485962355447,
+          "rotation": 0,
+          "target": "2-claut__"
+        },
+        {
+          "yaw": -2.108780952142695,
+          "pitch": 0.5379285519267984,
+          "rotation": 0,
+          "target": "0-claut__"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-claut__",
+      "name": "Claut_내부_선수대기실내부",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 1.574774028607881,
+        "pitch": 0.32461170945559203,
+        "fov": 1.3737605190682325
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3962975641015296,
+          "pitch": 0.32080555303940095,
+          "rotation": 0,
+          "target": "3-claut___"
+        },
+        {
+          "yaw": 0.8802371126529831,
+          "pitch": 0.17734307088757362,
+          "rotation": 0,
+          "target": "5-claut__"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-claut___",
+      "name": "Claut_내부_선수대기실_화장실",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.9409112417956891,
+        "pitch": 0.16953676021708475,
+        "fov": 1.3737605190682325
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6238051218475675,
+          "pitch": 0.30203721175871934,
+          "rotation": 0,
+          "target": "2-claut__"
+        },
+        {
+          "yaw": 0.03626521975630048,
+          "pitch": 0.2492916306869084,
+          "rotation": 0,
+          "target": "4-claut___"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-claut___",
+      "name": "Claut_내부_선수대기실_화장실",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5617658283581877,
+          "pitch": 0.2470658717908396,
+          "rotation": 0,
+          "target": "3-claut___"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-claut__",
+      "name": "Claut_내부_경기장출입구",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.06880011259028151,
+        "pitch": 0.11694009328832422,
+        "fov": 1.3737605190682325
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.071235299857255,
+          "pitch": 0.42619445262641875,
+          "rotation": 0,
+          "target": "2-claut__"
+        },
+        {
+          "yaw": -0.4813337367028936,
+          "pitch": 0.19698600308195857,
+          "rotation": 0,
+          "target": "6-claut__1"
+        },
+        {
+          "yaw": 0.7979572564872619,
+          "pitch": 0.18594689915293472,
+          "rotation": 0,
+          "target": "7-claut___1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-claut__1",
+      "name": "Claut_내부_컬링장내부1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.060987292478955624,
+        "pitch": 0.032478248813644583,
+        "fov": 1.3737605190682325
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5462855282913983,
+          "pitch": 0.3038570796642439,
+          "rotation": 0,
+          "target": "7-claut___1"
+        },
+        {
+          "yaw": -1.6374646868992873,
+          "pitch": 0.260495826224167,
+          "rotation": 0,
+          "target": "8-claut___2"
+        },
+        {
+          "yaw": 2.6899639727660194,
+          "pitch": 0.36565846032300087,
+          "rotation": 0,
+          "target": "5-claut__"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-claut___1",
+      "name": "Claut_내부_경기장_시트1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.8498524843996833,
+        "pitch": 0.21510841131499348,
+        "fov": 1.3737605190682325
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4412603599548888,
+          "pitch": 0.43434888655309756,
+          "rotation": 0,
+          "target": "6-claut__1"
+        },
+        {
+          "yaw": -1.6475410745988874,
+          "pitch": 0.2619563768587412,
+          "rotation": 0,
+          "target": "8-claut___2"
+        },
+        {
+          "yaw": -2.2431650348068537,
+          "pitch": 0.34851120390014856,
+          "rotation": 0,
+          "target": "5-claut__"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-claut___2",
+      "name": "Claut_내부_경기장_시트2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.526047399421568,
+        "pitch": 0.2700194074318514,
+        "fov": 1.3737605190682325
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.037869634978808,
+          "pitch": 0.3245293590345675,
+          "rotation": 0,
+          "target": "9-claut__"
+        },
+        {
+          "yaw": 1.301191739658421,
+          "pitch": 0.39443181110680214,
+          "rotation": 0,
+          "target": "6-claut__1"
+        },
+        {
+          "yaw": 1.5230460850377003,
+          "pitch": 0.058359449143040365,
+          "rotation": 0,
+          "target": "7-claut___1"
+        },
+        {
+          "yaw": 1.7814737026362968,
+          "pitch": 0.15257058211530605,
+          "rotation": 0,
+          "target": "5-claut__"
+        },
+        {
+          "yaw": -0.5250853137773568,
+          "pitch": 0.41102082552235153,
+          "rotation": 0,
+          "target": "10-claut__"
+        },
+        {
+          "yaw": -0.02283156090227756,
+          "pitch": 0.027699570981734922,
+          "rotation": 0,
+          "target": "12-claut__"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-claut__",
+      "name": "Claut_내부_경기장화장실",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0824350189123564,
+          "pitch": 0.44956667889999125,
+          "rotation": 0,
+          "target": "8-claut___2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-claut__",
+      "name": "Claut_내부_복도(아이스링크장)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -1.9944371433581942,
+        "pitch": 0.24900226383778978,
+        "fov": 1.3737605190682325
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.213296585780867,
+          "pitch": 0.34862890270662383,
+          "rotation": 0,
+          "target": "8-claut___2"
+        },
+        {
+          "yaw": -3.0918772143880524,
+          "pitch": 0.2369451304545951,
+          "rotation": 0,
+          "target": "11-claut__"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5144330719291403,
+          "pitch": 0.15521453546476138,
+          "title": "Claut_경기장 출입문",
+          "text": "아이스링크장 출입문"
+        }
+      ]
+    },
+    {
+      "id": "11-claut__",
+      "name": "Claut_내부_아이스링크장",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5582287861668114,
+          "pitch": 0.18402449826599288,
+          "rotation": 0,
+          "target": "10-claut__"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-claut__",
+      "name": "Claut_내부_컬링시트중간",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5531723105772635,
+          "pitch": 0.13261653285485941,
+          "rotation": 0,
+          "target": "8-claut___2"
+        },
+        {
+          "yaw": -1.6005448281378385,
+          "pitch": 0.06213495996559004,
+          "rotation": 0,
+          "target": "13-claut__"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-claut__",
+      "name": "Claut_내부_컬링시트끝",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.12874770663862378,
+        "pitch": 0.22506886147560579,
+        "fov": 1.3737605190682325
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6471999902753431,
+          "pitch": 0.12027131161539018,
+          "rotation": 0,
+          "target": "12-claut__"
+        },
+        {
+          "yaw": -0.7085246841917705,
+          "pitch": 0.10970099580110926,
+          "rotation": 0,
+          "target": "14-claut__2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-claut__2",
+      "name": "Claut_내부_컬링장내부2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.026583909153057306,
+        "pitch": -0.0021668146988460535,
+        "fov": 1.3737605190682325
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.023996713512584478,
+          "pitch": -0.022860430241436802,
+          "rotation": 0,
+          "target": "6-claut__1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "qtvr",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
